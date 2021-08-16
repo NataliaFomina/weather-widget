327bafc13c370b53d669fc39f3d7578d2b68f7fe
Natalia Fomina <fominatasha@gmail.com>
2021-08-16 07:31:23 +0100

    updated settings

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,36 @@ module.exports = {
   output: {
     filename: "bundle.js",
     sourceMapFilename: "bundle.js.map",
-    path: path.resolve(outputDir)
+    path: path.resolve(outputDir),
   },
   devServer: {
     contentBase: outputDir,
     writeToDisk: true,
   },
-}
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
